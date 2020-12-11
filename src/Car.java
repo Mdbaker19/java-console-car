d1209b01ecb7962a28f14ebe3d24c6e3cca81a2f
Matthew <matthew.d.baker20@gmail.com>
2020-12-11 12:46:49 -0600

    added other features of the console car, charging, using charge while driving, error message if trying to switch into reverse while speed is greater than 0

--- src/Car.java
+++ src/Car.java
@@ -3,11 +3,15 @@ public class Car {
     private String brand;
     private int speed;
     private boolean fuelType;
+    private boolean direction;
+    private int charge;
 
-    public Car(String brand, int speed, boolean ev) {
+    public Car(String brand, int speed, boolean ev, boolean direction, int charge) {
         this.brand = brand;
         this.speed = speed;
         this.fuelType = ev;
+        this.direction = direction;
+        this.charge = charge;
     }
 
     public String getBrand(){
@@ -22,6 +26,20 @@ public class Car {
     }
     public void setSpeed(int newSpeed){
         this.speed = newSpeed;
+    }
+
+    public void setDirection(){
+        this.direction = !this.direction;
+    }
+
+    public boolean getDirection(){
+        return this.direction;
+    }
+    public void setCharge(int charge){
+        this.charge = charge;
+    }
+    public int getCharge(){
+        return this.charge;
     }
 
 
--- src/CarTest.java
+++ src/CarTest.java
@@ -6,9 +6,9 @@ public class CarTest {
 
 
         boolean keepOperating = true;
-        Car myCar = new Car("Tesla", 0, true);
+        Car myCar = new Car("Tesla", 0, true, true, 100);
         do {
-            System.out.println("I am a car and I can do many things! [E]xit All This, [D]rive, [R]everse, [V]iew Brand, [S]peed Check");
+            System.out.println("I am a car and I can do many things! [E]xit All This, [D]rive, [R]everse, [V]iew Brand, [S]peed Check, [B]rake, [C]harge, [L]evel of Charge");
 
             String decision = sc.next();
 
@@ -21,10 +21,32 @@ public class CarTest {
             if (decision.equalsIgnoreCase("v")) {
                 System.out.println(myCar.getBrand());
             } else if(decision.equalsIgnoreCase("d")){
-                CarOperations.accelerate(myCar);
+                if(myCar.getCharge() > 0) {
+                    CarOperations.accelerate(myCar);
+                    myCar.setCharge((myCar.getCharge()) - 10);
+                } else {
+                    System.out.println("Out of charge");
+                }
             } else if(decision.equalsIgnoreCase("s")){
                 System.out.printf("Current speed: %d / mph%n", myCar.getSpeed());
+            } else if(decision.equalsIgnoreCase("r")){
+                if(myCar.getSpeed() > 0){
+                    System.err.println("Car must be stopped in order to switch gears");
+                } else if(myCar.getSpeed() == 0 && myCar.getDirection()){
+                    myCar.setDirection();
+                    System.out.println("In Reverse");
+                } else if(myCar.getSpeed() == 0 && !myCar.getDirection()){
+                    myCar.setDirection();
+                    System.out.println("In Drive");
+                }
+            } else if(decision.equalsIgnoreCase("b")){
+                myCar.setSpeed(0);
+            } else if(decision.equalsIgnoreCase("c")){
+                myCar.setCharge(100);
+            } else if(decision.equalsIgnoreCase("l")){
+                System.out.println("Current chage level: " + myCar.getCharge());
             }
+            System.out.println("=============");
 
         }while(keepOperating);
 
